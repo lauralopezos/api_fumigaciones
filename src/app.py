14feee40e779af6e599346df75f78776bbd23acc
lauralopezos <laurylopez87@gmail.com>
2025-08-27 18:39:10 -0500

    fix bugs 5

--- src/app.py
+++ src/app.py
@@ -1,14 +1,11 @@
-import os
 from flask import Flask
 from config import DevelopmentConfig
 from db import mysql
 from routes import consumidores_bp, tecnicos_bp, administradores_bp
-from ui import ui as ui_bp 
+from ui import ui as ui_bp
 
 app = Flask(__name__)
-
-env = os.getenv("APP_ENV", "development")
-app.config.from_object(DevelopmentConfig) 
+app.config.from_object(DevelopmentConfig)
 
 mysql.init_app(app)
 
@@ -16,7 +13,3 @@ app.register_blueprint(consumidores_bp)
 app.register_blueprint(tecnicos_bp)
 app.register_blueprint(administradores_bp)
 app.register_blueprint(ui_bp)
-
-@app.errorhandler(404)
-def not_found(e):
-    return "<h1>La página no existe</h1>", 404
--- src/config.py
+++ src/config.py
@@ -1,8 +1,6 @@
-import os
-
 class DevelopmentConfig:
     DEBUG = True
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
-    MYSQL_USER = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-    MYSQL_DB = os.getenv("MYSQL_DB", "fumigaciones_db")
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'fumigaciones_db'
