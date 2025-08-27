c08437c237454f6f310a86558c86d60aa8a33c88
lauralopezos <laurylopez87@gmail.com>
2025-08-27 18:50:24 -0500

    fix bugs 6

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,21 @@
-import os, sys, pytest
-from pathlib import Path
-
-# src
-BASE_DIR = Path(__file__).resolve().parents[1]
-SRC_DIR = BASE_DIR / "src"
-if str(SRC_DIR) not in sys.path:
-    sys.path.insert(0, str(SRC_DIR))
-
-os.environ["APP_ENV"] = "testing"
+import os
+import importlib
 
 
+os.environ.setdefault("MYSQL_HOST", "127.0.0.1")
+os.environ.setdefault("MYSQL_USER", "root")
+os.environ.setdefault("MYSQL_PASSWORD", "root")
+os.environ.setdefault("MYSQL_DB", "fumigaciones_db")
+
+
+import config
+config.DevelopmentConfig.MYSQL_HOST = os.environ["MYSQL_HOST"]
+config.DevelopmentConfig.MYSQL_USER = os.environ["MYSQL_USER"]
+config.DevelopmentConfig.MYSQL_PASSWORD = os.environ["MYSQL_PASSWORD"]
+config.DevelopmentConfig.MYSQL_DB = os.environ["MYSQL_DB"]
+
+
+import pytest
 from app import app
 from db import mysql
 from MySQLdb import OperationalError
