d30b837fafbc8083fd42554360bae59f9a5fdfb6
lauralopezos <laurylopez87@gmail.com>
2025-08-27 16:53:49 -0500

    Fix bugs 3

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
-class DevelopmentConfig():
+import os
+
+class DevelopmentConfig:
     DEBUG = True
-    MYSQL_HOST = '127.0.0.1'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'fumigaciones_db'
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
+    MYSQL_DB = os.getenv("MYSQL_DB", "fumigaciones_db")
