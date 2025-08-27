d6a8024fca13ded7ccd81585faaf6445b44a75b0
lauralopezos <laurylopez87@gmail.com>
2025-08-27 18:34:37 -0500

    fix bugs 4

--- src/app.py
+++ src/app.py
@@ -1,30 +1,22 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from config import DevelopmentConfig
 from db import mysql
 from routes import consumidores_bp, tecnicos_bp, administradores_bp
-from ui import ui
+from ui import ui as ui_bp 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TPL_DIR  = os.path.join(BASE_DIR, "templates")
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder=TPL_DIR)
-app.config.from_object(DevelopmentConfig['development'])
+env = os.getenv("APP_ENV", "development")
+app.config.from_object(DevelopmentConfig) 
 
 mysql.init_app(app)
-
-@app.get("/health")
-def health():
-    return jsonify({"status": "ok"})
 
 app.register_blueprint(consumidores_bp)
 app.register_blueprint(tecnicos_bp)
 app.register_blueprint(administradores_bp)
+app.register_blueprint(ui_bp)
 
-
-app.register_blueprint(ui)
-
-if __name__ == "__main__":
-    print("\n== RUTAS REGISTRADAS ==")
-    print(app.url_map)
-    app.run(debug=True)
+@app.errorhandler(404)
+def not_found(e):
+    return "<h1>La página no existe</h1>", 404
