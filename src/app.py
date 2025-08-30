9ab50c17dfda70852cee121e25a8fd7fb9d77d88
lauralopezos <laurylopez87@gmail.com>
2025-08-30 01:56:27 -0500

    Arreglos

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
+from flask import Flask, redirect, url_for
+from src.db import init_app as init_db
+from src.routes import bp as api_bp
+from src.ui import bp as ui_bp
 import os
-from flask import Flask
-from src.db import init_app as init_db 
-from src.routes import bp as api_bp          
-from src.ui import bp as ui_bp                
-
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -11,11 +10,19 @@ def create_app() -> Flask:
     if not db_url:
         raise RuntimeError("DATABASE_URL no está definida")
     app.config["DATABASE_URL"] = db_url
+
     init_db(app)
+    app.register_blueprint(api_bp)
+    app.register_blueprint(ui_bp)
 
     @app.get("/ping")
     def ping():
         return {"status": "ok"}
+
+    @app.get("/") 
+    def root():
+        return redirect(url_for("ui.index"))
+
     return app
 
 app = create_app()
